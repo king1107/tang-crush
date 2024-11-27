3c9a35a75cc0eddfdc7631b3085c5401e7aa9913
Trang Minh Trí <86817635+king1107@users.noreply.github.com>
2024-11-27 19:19:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 const CONFIG = {
     introTitle: 'He luu cậu!',
-    introDesc: `Tớ có điều này muốn hỏi
+    introDesc: `Tớ có điều này muốn hỏi - 
     Cậu phải trả lời thật lòng nhaaa`,
     btnIntro: 'hihi',
-    title: 'Cậu yêu tớ phải không nào',
-    desc: 'Nếu cậu ko trả lời mà thoát ra là muốn làm vợ rùi đó nha :v ',
+    title: 'Cậu thích tớ phải không nào',
+    desc: 'Nếu cậu ko trả lời mà thoát ra là muốn làm vợ tớ rùi đó nha :v ',
     btnYes: 'Yêu ơi là yêuuuuuuuu <333',
     btnNo: 'Cậu mơ à :33',
     question:'Nói cho tớ lí do cậu thích tớ đi :vvvvv ',
     btnReply: 'Gửi cho tớ <3',
-    reply: 'Yêu cậu ko cần lí do <333333',
+    reply: 'Thích cậu ko cần lí do <3',
     mess: 'Tớ biết mà ^^ Yêu cậu 3000 🥰',
     messDesc: 'Tối nay tớ qua đón cậu đi chơi nhaaaaaa :V Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàoooooooo.',
     btnAccept: 'Okiiiiii lun <3',
